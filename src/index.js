8f078680a206379bda0c3c8d6f6ee4f5726f7e51
gopienkonikita <nikita.gopienko@onlyoffice.com>
2019-12-16 12:39:31 +0300

    added import themes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Text } from './components/Text'
 export { default as Box } from './components/Box'
 export { default as ThemeProvider } from './components/Theme-provider'
+export * as Themes from './themes'
