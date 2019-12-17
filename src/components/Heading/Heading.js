2f77671020c5556652e7155981a649d3bdf7943d
gopienkonikita <nikita.gopienko@onlyoffice.com>
2019-12-17 14:06:33 +0300

    Added Heading component

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledHeading from "./StyledHeading";
+
+const Heading = ({ level, ...rest }) => {
+  //console.log("Heading render", rest)
+  return <StyledHeading as={`h${level}`} {...rest} />;
+};
+
+Heading.propTypes = {
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+  color: PropTypes.string,
+  title: PropTypes.string,
+  truncate: PropTypes.bool,
+  isInline: PropTypes.bool,
+  size: PropTypes.oneOf(["xsmall", "small", "medium", "large", "xlarge"]),
+  className: PropTypes.string
+};
+
+Heading.defaultProps = {
+  title: "",
+  truncate: false,
+  isInline: false,
+  size: "large",
+  level: 1,
+  className: ""
+};
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Text } from "./components/Text";
 export { default as Box } from "./components/Box";
 export { default as ThemeProvider } from "./components/ThemeProvider";
+export { default as Heading } from "./components/Heading";
 export * as Themes from "./themes";
