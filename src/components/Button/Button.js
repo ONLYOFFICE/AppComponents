81528838ff36c4e56d39b0869604739af359eee3
gopienkonikita <nikita.gopienko@onlyoffice.com>
2019-12-24 15:26:55 +0300

    Removed icon from button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,27 +3,6 @@ import PropTypes from "prop-types";
 import isEqual from "lodash/isEqual";
 import StyledButton from "./StyledButton";
 
-const Icon = ({ size, primary, icon }) => (
-  <div className="btnIcon">
-    {icon &&
-      React.cloneElement(icon, {
-        isfill: true,
-        size: size === "big" ? "medium" : "small",
-        color: primary ? "#FFFFFF" : "#333333"
-      })}
-  </div>
-);
-
-Icon.propTypes = {
-  icon: PropTypes.node,
-  size: PropTypes.string,
-  primary: PropTypes.bool
-};
-
-Icon.defaultProps = {
-  icon: null
-};
-
 class Button extends React.Component {
   shouldComponentUpdate(nextProps) {
     return !isEqual(this.props, nextProps);
@@ -31,14 +10,9 @@ class Button extends React.Component {
 
   render() {
     //console.log("Button render", this.props);
-    const { label, icon } = this.props;
+    const { label } = this.props;
 
-    return (
-      <StyledButton {...this.props}>
-        {icon ? <Icon {...this.props} /> : null}
-        {label}
-      </StyledButton>
-    );
+    return <StyledButton {...this.props}>{label}</StyledButton>;
   }
 }
 
@@ -47,7 +21,6 @@ Button.propTypes = {
   primary: PropTypes.bool,
   size: PropTypes.oneOf(["base", "medium", "big"]),
   scale: PropTypes.bool,
-  icon: PropTypes.node,
   tabIndex: PropTypes.number,
   className: PropTypes.string,
   id: PropTypes.string,
@@ -61,7 +34,6 @@ Button.defaultProps = {
   primary: false,
   size: "base",
   scale: false,
-  icon: null,
   tabIndex: -1,
   disabled: false
 };
