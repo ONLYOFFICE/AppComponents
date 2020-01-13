b87d621201a4a09cb7926773b9cd8daab929f370
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-01-13 13:30:38 +0300

    Added radio-button import

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ export { default as Heading } from "./components/Heading";
 export { default as Button } from "./components/Button";
 export { default as Checkbox } from "./components/Checkbox";
 export { default as ToggleButton } from "./components/ToggleButton";
+export { default as RadioButton } from "./components/RadioButton";
 export * as Themes from "./themes";
