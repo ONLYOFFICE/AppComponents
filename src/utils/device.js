03582661c3f055e0cc8032d625a1b32f566e15a3
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-01-23 09:36:02 +0300

    Added device utils

---
+++ src/utils/device.js
@@ -0,0 +1,11 @@
+const size = {
+  mobile: "375px",
+  tablet: "1024px",
+  desktop: "1025px"
+};
+
+export const mobile = `(max-width: ${size.mobile})`;
+
+export const tablet = `(max-width: ${size.tablet})`;
+
+export const desktop = `(min-width: ${size.desktop})`;
