b0c5ab6658784b1787294f933029e3515545bb70
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-01-30 13:44:45 +0300

    Added tests to ToggleContent, fixed styles

--- src/components/ToggleContent/StyledToggleContent.js
+++ src/components/ToggleContent/StyledToggleContent.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { IconArrow } from "./svg";
 import { NoUserSelect } from "../../utils/commonStyles";
+import { Base } from "../../themes";
 
 const StyledContainer = styled.div`
   .span-toggle-content {
@@ -31,9 +32,13 @@ const StyledContent = styled.div`
   padding: ${props => props.theme.toggleContent.contentPadding};
 `;
 
-const Arrow = styled(IconArrow)`
+const StyledArrow = styled(IconArrow)`
   margin: ${props => props.theme.toggleContent.arrowMargin};
   transform: ${props => props.open && props.theme.toggleContent.transform};
 `;
 
-export { StyledContainer, StyledContent, Arrow };
+StyledContainer.defaultProps = { theme: Base };
+StyledContent.defaultProps = { theme: Base };
+StyledArrow.defaultProps = { theme: Base };
+
+export { StyledContainer, StyledContent, StyledArrow };
