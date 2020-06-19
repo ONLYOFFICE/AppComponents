b6b6123935f2aee550433f4283637b6c0dbf053a
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-06-19 11:41:20 +0300

    Add new tests with jest-styled-components

--- src/components/PhoneInput/PhoneInput.test.js
+++ src/components/PhoneInput/PhoneInput.test.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { mount } from "enzyme";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
 import PhoneInput from ".";
+import { StyledDropDown, StyledCountryItem, StyledSearchPanel } from "./StyledPhoneInput";
+import ThemeProvider from "../ThemeProvider";
+import { Base } from "../../themes";
 
 const defaultProps = {
     locale: "RU",
     value: "12",
     onChange: jest.fn()
 };
+
+jest.mock('react-text-mask', () => props => <input type="text" {...{ ...props }} />)
+
 describe("<PhoneInput />", () => {
 
     it("component renders without error", () => {
@@ -76,5 +84,50 @@ describe("<PhoneInput />", () => {
             <PhoneInput placeholderColor="#fff" />
         );
         expect(wrapper.prop('placeholderColor')).toEqual('#fff');
+    });
+
+    it('should applies theme props', () => {
+        const phoneInput = renderer
+        .create(
+            <ThemeProvider theme={Base}>
+                <PhoneInput {...defaultProps} />
+            </ThemeProvider>
+        )
+        .toJSON();
+
+        expect(phoneInput).toHaveStyleRule("width", Base.phoneInput.width);
+    });
+
+    it('should applies theme props to nested styled components', () => {
+        const styledDropDown = renderer.create(<StyledDropDown />).toJSON();
+        expect(styledDropDown).toHaveStyleRule("width", Base.phoneInput.width);
+
+        const styledCountryItem = renderer.create(<StyledCountryItem />).toJSON();
+        expect(styledCountryItem).toHaveStyleRule("background-color", Base.phoneInput.itemBackgroundColor);
+        
+        const styledSearchPanel = renderer.create(<StyledSearchPanel />).toJSON();
+        expect(styledSearchPanel).toHaveStyleRule("color", Base.phoneInput.placeholderColor, {
+            modifier: '.phone-input-searcher::placeholder'
+        });
+    });
+
+      it('should applies new theme', () => {
+
+      const newTheme = Object.assign({}, Base, {
+          phoneInput: {
+            width: "100px"
+          }
+        }
+      );
+
+      const phoneInput2 = renderer
+        .create(
+            <ThemeProvider theme={newTheme}>
+                <PhoneInput {...defaultProps} />
+            </ThemeProvider>
+        )
+        .toJSON();
+
+        expect(phoneInput2).toHaveStyleRule("width", newTheme.phoneInput.width);
     });
 });
