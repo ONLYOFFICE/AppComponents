391ad1da0c23ed0b5cf3770403c2f6b2a2ea2f23
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-06-23 15:36:36 +0300

    SquareButton init

---
+++ src/components/SquareButton/SquareButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import isEqual from "lodash/isEqual";
+import { StyledSquareButton, StyledCrossShape } from "./StyledSquareButton";
+
+class SquareButton extends React.Component {
+
+  shouldComponentUpdate(nextProps) {
+    return !isEqual(this.props, nextProps);
+  }
+
+  render() {
+
+    console.log("Button render");
+
+    return (
+      <StyledSquareButton  {...this.props}>
+        <StyledCrossShape />
+      </StyledSquareButton>
+    );
+  }
+}
+
+SquareButton.propTypes = {
+  tabIndex: PropTypes.number,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func
+};
+
+SquareButton.defaultProps = {
+  tabIndex: -1,
+  disabled: false
+};
+
+export default SquareButton;
